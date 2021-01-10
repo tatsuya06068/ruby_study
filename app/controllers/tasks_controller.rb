fa967a330fb1cdcf3b7b632b4adfa8979d795c7d
tatsuya06068 <fhkt863@gmail.com>
2021-01-10 13:26:32 +0900

    新規登録確認画面追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,11 +14,22 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params.merge(user_id: current_user.id))
+
+    if params[:back].present?
+      render :new
+      return
+    end
+    
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   def edit
